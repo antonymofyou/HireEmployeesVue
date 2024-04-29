fffbbe9888d4df8b0872df32ba753fbf58e347fb
antonymofyou <89082893+antonymofyou@users.noreply.github.com>
2024-04-29 15:56:57 +0300

    Первый коммит проекта

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+<h1>Новый проект</h1>
+</template>
+
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+//import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<style scoped>
+
+</style>
