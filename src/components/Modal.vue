782057c7972409ca9ef44fded50f9b8833c040d2
Denis <cblick@yandex.com>
2024-05-25 13:52:50 +0300

    fix: обновление модального окна

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,87 +1,79 @@
 <template>
-    <Transition name="modal">
-        <div v-if="show" class="modal-mask">
-            <div class="modal-container">
-                <div class="modal-header">
-                    <slot name="header"></slot>
-                </div>
-
-                <div class="modal-body">
-                    <slot name="body"></slot>
-                </div>
-
-                <div class="modal-footer">
-                    <slot name="footer-control-buttons">
-                    </slot>
-                    <CustomButton @click="$emit('close')" :button-color="buttonColor" :text-color="textColor">
-                        <template #text>
-                            <slot name="close-button-text">
-                            </slot>
-                        </template>
-                    </CustomButton>
-
-
-                </div>
-            </div>
+  <Transition name="modal">
+    <div v-if="show" class="modal-mask">
+      <div class="modal-container">
+        <div class="modal-header">
+          <slot name="header"></slot>
         </div>
-    </Transition>
+
+        <div class="modal-body">
+          <slot name="body"></slot>
+        </div>
+
+        <div class="modal-footer">
+          <slot name="footer-control-buttons"> </slot>
+        </div>
+      </div>
+    </div>
+  </Transition>
 </template>
 
 <script setup>
-import CustomButton from './CustomButton.vue';
 
 const props = defineProps({
-    show: Boolean
+  show: Boolean,
 });
 
-const buttonColor = "var(--transparent-blue)";
-const textColor = "var(--white)";
 </script>
 
 <style scoped>
 .modal-mask {
-    position: fixed;
-    z-index: 9998;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background-color: rgba(0, 0, 0, 0.5);
-    display: flex;
-    transition: opacity 0.3s ease;
+  position: fixed;
+  z-index: 9998;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  transition: opacity 0.3s ease;
 }
 
 .modal-container {
-    width: 300px;
-    margin: auto;
-    padding: 20px 30px;
-    background-color: #fff;
-    border-radius: 10px;
-    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
-    transition: all 0.3s ease;
+  width: 300px;
+  margin: auto;
+  padding: 20px 30px;
+  background-color: #fff;
+  border-radius: 10px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
+  transition: all 0.3s ease;
 }
 
 .modal-header h3 {
-    margin-top: 0;
-    color: #42b983;
+  margin-top: 0;
+  color: #42b983;
 }
+
 .modal-header {
-    text-align: center;
+  text-align: center;
 }
 
 .modal-body {
-    margin: 20px 0;
+  margin: 20px 0;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
 }
 
 .modal-default-button {
-    float: right;
+  float: right;
 }
 
 .modal-footer {
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-    gap:10px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  gap: 10px;
 }
 
 /*
@@ -94,16 +86,16 @@ const textColor = "var(--white)";
  */
 
 .modal-enter-from {
-    opacity: 0;
+  opacity: 0;
 }
 
 .modal-leave-to {
-    opacity: 0;
+  opacity: 0;
 }
 
 .modal-enter-from .modal-container,
 .modal-leave-to .modal-container {
-    -webkit-transform: scale(1.1);
-    transform: scale(1.1);
+  -webkit-transform: scale(1.1);
+  transform: scale(1.1);
 }
 </style>
