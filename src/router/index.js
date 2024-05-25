935f06d07e9f6ec98c97441ee58ce5bf4d1f3540
Denis <cblick@yandex.com>
2024-05-25 16:52:43 +0300

    fix: исправление верстки страницы вакансий, дописание функционала добавления вакансий

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomePage from "@/pages/HomePage.vue";
 import ManagerAuth from "@/pages/authorization/ManagerAuth.vue";
 import VacanciesList from "@/pages/vacancy/VacanciesPage/VacanciesList.vue";
-import VacancyIdPage from "@/pages/vacancy/VacanciesPage/components/VacancyIdPage.vue";
+import VacancyIdPage from "@/pages/vacancy/VacanciesPage/VacancyIdPage.vue";
 import VacancyEditPage from "@/pages/vacancy/EditPage/VacancyEditPage.vue";
 import CandidatesList from "@/pages/candidates/CandidatesList.vue";
 import CandidatComments from "@/pages/candidates/CandidatComments.vue";
