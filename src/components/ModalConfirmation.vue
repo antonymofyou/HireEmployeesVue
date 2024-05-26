4a9a90b917774771274f967328d8cb39d54f62d9
Artur Makars <adementor@yandex.ru>
2024-05-27 02:42:11 +0300

    Реализовано модальное окно подтверждения

---
+++ src/components/ModalConfirmation.vue
@@ -0,0 +1,115 @@
+<template>
+  <div>
+  <Modal :show="show">
+    <template v-slot:header>
+      <h3 class="modal-confirmation__title">{{ title }}</h3>
+    </template>
+
+    <template v-slot:body>
+      <div class="modal-confirmation__body">
+        <p>{{ text }}</p>
+      </div>
+    </template>
+
+    <template v-slot:footer-control-buttons>
+      <div class="modal-confirmation__controls">
+        <ButtonSimple
+          @click="$emit('cancel')"
+          :textColor="cancelTextColor"
+          :buttonColor="cancelButtonColor"
+          :isBold=true
+        >
+          <template v-slot:text>
+            {{ cancelText }}
+          </template>
+        </ButtonSimple>
+        <ButtonSimple
+          @click="$emit('confirm')"
+          :textColor="confirmTextColor"
+          :buttonColor="confirmButtonColor"
+          :isBold=true
+        >
+          <template v-slot:text>
+            {{ confirmText }}
+          </template>
+        </ButtonSimple>
+      </div>
+    </template>
+
+  </Modal>
+</div>
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+import Modal from '@/components/Modal.vue';
+import ButtonSimple from '@/components/ButtonSimple.vue';
+
+// Показ модального окна, заголовок, текст, текст подтверждения, текст отмены,
+// цвет текста подтверждения, цвет текста отмены, цвет кнопки подтверждения, цвет кнопки отмены
+const props = defineProps({
+  show: {
+    type: Boolean,
+  },
+  title: {
+    type: String,
+    default: 'Подтверждение',
+  },
+  text: {
+    type: String,
+  },
+  confirmText: {
+    type: String,
+    default: 'Отправить',
+  },
+  cancelText: {
+    type: String,
+    default: 'Отмена',
+  },
+  confirmTextColor: {
+    type: String,
+    default: defaultConfirmTextColor,
+  },
+  confirmButtonColor: {
+    type: String,
+    default: defaultConfirmBtnColor,
+  },
+  cancelTextColor: {
+    type: String,
+    default: defaultCancelTextColor,
+  },
+  cancelButtonColor: {
+    type: String,
+    default: defaultCancelBtnColor,
+  },
+});
+
+//Значения по умолчанию для кастомизации кнопок управления
+const defaultConfirmBtnColor = 'var(--cornflower-blue)';
+const defaultConfirmTextColor = 'var(--white)';
+const defaultCancelBtnColor = 'var(--link-water)';
+const defaultCancelTextColor = 'var(--cornflower-blue)';
+</script>
+
+<style scoped>
+.modal-confirmation__title {
+  font-size: 20px;
+  font-weight: 600;
+
+  margin: 0;
+}
+
+.modal-confirmation__body {
+  max-width: 400px;
+  min-height: 80px;
+
+  text-align: center;
+}
+
+.modal-confirmation__controls {
+  display: flex;
+  gap: 10px;
+  
+  margin-left: auto;
+}
+</style>
