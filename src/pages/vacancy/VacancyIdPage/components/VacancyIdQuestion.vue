0b9f39810472a93d8cb3c7475bd9104cef55e690
Artur Makars <adementor@yandex.ru>
2024-05-27 02:43:06 +0300

    Реализована страница заполнения вакансии соискателем

---
+++ src/pages/vacancy/VacancyIdPage/components/VacancyIdQuestion.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="question">
+    <span class="question__title">{{ title }}</span>
+    <div class="question__text" v-html="question"></div>
+    <InputSimple
+      :modelValue="answer"
+      @update:modelValue="updateAnswer"
+      inputType="textarea"
+      size="extra-medium"
+      placeholder="Введите ваш ответ..."
+      :disabled="disabled"
+    />
+  </div>
+</template>
+
+<script setup>
+import InputSimple from '@/components/InputSimple.vue';
+
+import { ref } from 'vue';
+
+// заголовок вопроса, текст вопроса, текст ответа, disabled
+const props = defineProps({
+    title: {
+      type: String,
+    },
+    question: {
+      type: String,
+      required: true,
+    },
+    answer: {
+      type: String,
+      default: '',
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    }
+});
+
+// Значение ответа
+const answer = ref(props.answer);
+
+// Обновление данных в родителе
+const emit = defineEmits(['updateAnswer']);
+
+// Обновление текста
+const updateAnswer = (newValue) => {
+  answer.value = newValue;
+  emit('updateAnswer', newValue);
+};
+</script>
+
+<style scoped>
+.question__title {
+  font-weight: 600;
+}
+
+.question__text {
+  margin-top: 10px;
+  margin-bottom: 10px;
+}
+</style>
