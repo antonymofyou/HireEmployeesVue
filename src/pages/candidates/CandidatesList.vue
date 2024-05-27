43ae1978dfcaa3307c1068925727994da8e0bb6e
Denis <cblick@yandex.com>
2024-05-27 18:13:42 +0300

    feature: Написание базового функционала отображения кандидатов по id вакансии

--- src/pages/candidates/CandidatesList.vue
+++ src/pages/candidates/CandidatesList.vue
@@ -1,11 +1,76 @@
 <template>
-  <div>
-    <h1>Лист кандидатов</h1>
-  </div>
+  <section class="candidates">
+    <h1 class="candidates__title">Лист кандидатов</h1>
+    <InputSimple
+      v-model="vacancyId"
+      id="vacancyId"
+      labelName="ID вакансии"
+      inputType="input"
+      @keydown.enter.self="getAllCandidatesManager"
+    />
+    <div v-if="!candidates">Вакансия не найдена</div>
+    <div v-else>Всего кандидатов: {{ candidates.length }}</div>
+
+    <div class="candidates__box-candidates">
+      <CandidateCard
+        v-for="candidate in candidates"
+        :key="candidate.candidateId"
+        :candidate="candidate"
+      />
+    </div>
+  </section>
 </template>
 
-<script>
-export default {};
+<script setup>
+import { onMounted, ref } from "vue";
+import { useRouter } from "vue-router";
+import { isManager } from "@/js/AuthFunctions";
+import { MainRequestClass } from "@/js/RootClasses";
+import InputSimple from "@/components/InputSimple.vue";
+import CandidateCard from "./components/CandidateCard.vue";
+
+const router = useRouter();
+
+// Ref-переменные
+const candidates = ref([]); // список кандидатов
+const vacancyId = ref(""); // ID вакансии
+
+//Проверка авторизации пользователя
+if (!isManager()) router.push({ name: "home" });
+
+// получение списка кандидатов по id вакансии
+function getAllCandidatesManager() {
+  class CandidatesGetCandidatesByVacancyId extends MainRequestClass {
+    vacancyId = vacancyId.value; // ID вакансии, отклики для которой нужно получить
+  }
+  let requestClass = new CandidatesGetCandidatesByVacancyId();
+
+  requestClass.request(
+    "/candidates/get_candidates_by_vacancy_id.php",
+    "manager",
+    function (response) {
+      //успешный результат
+      candidates.value = response.candidates;
+    },
+    function (err) {
+      //неуспешный результат
+      alert(err);
+    }
+  );
+}
+
+//Получение списка кандидатов при загрузке страницы
+onMounted(() => {
+  getAllCandidatesManager();
+});
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+.candidates {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 60px;
+  padding-bottom: 80px;
+}
+</style>
---
+++ src/pages/candidates/components/CandidateCard.vue
@@ -0,0 +1,22 @@
+<template>
+    <article class="candidate">
+      <div class="candidate__id">id: {{ candidate.candidateId }}</div>
+      <div class="candidate__description">
+        <div class="candidate__name">{{ candidate.fio }}</div>
+        <div class="candidate__tg">Telegram никнейм: {{ candidate.tgNickname }}, ID: {{ candidate.tgId }}</div>
+        <div class="candidate__date">Дата регистрации:{{ candidate.createdAt }}</div>
+        <div class="candidate__btn">Ответить</div>
+      </div>
+    </article>
+  </template>
+
+  <script setup>
+  //Объявление пропса
+  const props = defineProps(["candidate"]);
+  
+  </script>
+  
+  <style scoped>
+
+  </style>
+  
