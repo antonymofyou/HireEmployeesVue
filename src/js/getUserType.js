710269f22fd6bdfaf6750d626df129e1153c1bc0
Artur Makars <adementor@yandex.ru>
2024-07-06 03:50:39 +0300

    Удалены устаревшие/неиспользуемые файлы

--- src/js/getUserType.js
+++
@@ -1,9 +0,0 @@
-import { configData } from "./configData.js";
-
-export function getUserType(){
-    if(localStorage.getItem(configData.MANAGER_DEVICE_NAME)) return "manager";
-    else if(localStorage.getItem(configData.MANAGER_DEVICE_NAME)) return "seeker";
-    else {
-        return false;
-    }
-}
