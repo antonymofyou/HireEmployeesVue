e77bc372bfccfc66e14baa5dd74cbc792eb0cd7c
Sergeev Danila <Danilasergeev2003@icloud.com>
2024-08-03 17:40:43 +0300

    add click-outside and fix after review

--- src/pages/vacancy/VacancyEdit/components/VacancyManagers/VacancyManagers.vue
+++ src/pages/vacancy/VacancyEdit/components/VacancyManagers/VacancyManagers.vue
@@ -1,162 +1,159 @@
 <template>
-	<SpinnerMain
-		v-if="request"
-		class="manager-list__status-spinner"
-	/>
-	<VacancyManagersList
-		v-if="!request"
-		:managerList="managerList.assignedManagers"
-		:requestManagersModification
-		:indicators
-		:managerMod
-	></VacancyManagersList>
+  <SpinnerMain v-if="request" class="manager-list__status-spinner" />
+  <VacancyManagersList
+    v-if="!request"
+    :managerList="managerList.assignedManagers"
+    :requestManagersModification
+    :indicators
+    :managerMod
+  ></VacancyManagersList>
 
-	<Teleport to="body">
-		<!-- Вывод модалки для добавления и изменения статуса -->
-		<VacancyManagersModal
-			:show="indicators.isAdd || indicators.isDelete"
-			:managerMod
-			:indicators
-			:managerList="managerList.unassignedManagers"
-			:requestManagersModification
-			:request
-			:errorMessage
-			:formData
-		/>
-	</Teleport>
+  <Teleport to="body">
+    <!-- Вывод модалки для добавления и изменения статуса -->
+    <VacancyManagersModal
+      :show="indicators.isAdd || indicators.isDelete"
+      :managerMod
+      :indicators
+      :managerList="managerList.unassignedManagers"
+      :requestManagersModification
+      :request
+      :errorMessage
+      :formData
+    />
+  </Teleport>
 </template>
 
 <script setup>
-import SpinnerMain from '@/components/SpinnerMain.vue';
-import VacancyManagersModal from './VacancyManagersModal.vue';
+import SpinnerMain from "@/components/SpinnerMain.vue";
+import VacancyManagersModal from "./VacancyManagersModal.vue";
 
 import {
-	VacanciesAccessGetManagerAccessVacancy,
-	VacanciesAccessSetManagerAccessVacancy,
-} from '../../js/ApiClassesVacancyEdit';
-import { onMounted, watch, ref } from 'vue';
-import VacancyManagersList from './VacancyManagersList.vue';
+  VacanciesAccessGetManagerAccessVacancy,
+  VacanciesAccessSetManagerAccessVacancy,
+} from "../../js/ApiClassesVacancyEdit";
+import { onMounted, watch, ref } from "vue";
+import VacancyManagersList from "./VacancyManagersList.vue";
 
 const props = defineProps({
-	// Id вакансии
-	vacancyId: {
-		type: String,
-		required: true,
-	},
+  // Id вакансии
+  vacancyId: {
+    type: String,
+    required: true,
+  },
 });
 // Доступные статусы - общие, трансферные, для селекта
 const managerList = ref({
-	assignedManagers: [],
-	unassignedManagers: [],
+  assignedManagers: [],
+  unassignedManagers: [],
 });
 // Информация об изменяемом менеджере
 const managerMod = ref({
-	action: '',
-	permissionType: 'VACANCY_PERMISSION',
-	managerId: '',
+  action: "",
+  permissionType: "VACANCY_PERMISSION",
+  managerId: "",
 });
 //Данные с MainSelect
 const formData = ref({
-	id: '',
+  id: "",
 });
 
 //Индикаторы для открытия попапа и символа крестика
 const indicators = ref({
-	isDelete: false,
-	isAdd: false,
-	isHandled: false,
+  isDelete: false,
+  isAdd: false,
+  isHandled: false,
 });
 
 // Флаг запроса
 const request = ref(false);
 // Сообщение об ошибке
 const errorMessage = ref({
-	error: '',
+  error: "",
 });
 
 // Запрос менеджеров по id
 const requestVacancyManagers = () => {
-	const requestInstance = new VacanciesAccessGetManagerAccessVacancy();
-	requestInstance.vacancyId = props.vacancyId;
-	requestInstance.permissionType = managerMod.value.permissionType;
-	request.value = true;
-	errorMessage.value.error = '';
-	requestInstance.request(
-		'/vacancies/access/get_managers_access_vacancy.php',
-		'manager',
-		(response) => {
-			// Обновляем списки менеджеров
-			managerList.value.assignedManagers = response.assignedManagers;
-			managerList.value.unassignedManagers = response.unassignedManagers;
-			request.value = false;
-		},
-		(err) => {
-			request.value = false;
-			errorMessage.value.error = err;
-		}
-	);
+  const requestInstance = new VacanciesAccessGetManagerAccessVacancy();
+  requestInstance.vacancyId = props.vacancyId;
+  requestInstance.permissionType = managerMod.value.permissionType;
+  request.value = true;
+  errorMessage.value.error = "";
+  requestInstance.request(
+    "/vacancies/access/get_managers_access_vacancy.php",
+    "manager",
+    (response) => {
+      // Обновляем списки менеджеров
+      managerList.value.assignedManagers = response.assignedManagers;
+      managerList.value.unassignedManagers = response.unassignedManagers;
+      request.value = false;
+    },
+    (err) => {
+      request.value = false;
+      errorMessage.value.error = err;
+    }
+  );
 };
 
 // Запрос на добавление/удаление менеджера
 const requestManagersModification = (action, managerId) => {
-	const requestInstance = new VacanciesAccessSetManagerAccessVacancy();
-	//Условие нужно для проверки удаляем или добавляем
-	managerMod.value.managerId = managerId;
+  const requestInstance = new VacanciesAccessSetManagerAccessVacancy();
+  //Условие нужно для проверки удаляем или добавляем
+  managerMod.value.managerId = managerId;
 
-	managerMod.value.action = action;
-	requestInstance.action = managerMod.value.action;
-	requestInstance.vacancyId = props.vacancyId;
-	requestInstance.action = managerMod.value.action;
-	requestInstance.permissionType = managerMod.value.permissionType;
-	requestInstance.managerId = managerMod.value.managerId;
-	request.value = true;
-	errorMessage.value.error = '';
-	requestInstance.request(
-		'/vacancies/access/set_manager_access_vacancy.php',
-		'manager',
-		(response) => {
-			// Обновляем данные менеджеров
-			requestVacancyManagers();
-			indicators.value.isAdd = false;
-			indicators.value.isDelete = false;
-			formData.value.id = '';
-			managerMod.value.managerId = '';
-		},
-		(err) => {
-			request.value = false;
-			errorMessage.value.error = err;
-		}
-	);
+  managerMod.value.action = action;
+  requestInstance.action = managerMod.value.action;
+  requestInstance.vacancyId = props.vacancyId;
+  requestInstance.action = managerMod.value.action;
+  requestInstance.permissionType = managerMod.value.permissionType;
+  requestInstance.managerId = managerMod.value.managerId;
+  request.value = true;
+  errorMessage.value.error = "";
+  requestInstance.request(
+    "/vacancies/access/set_manager_access_vacancy.php",
+    "manager",
+    (response) => {
+      // Обновляем данные менеджеров
+      requestVacancyManagers();
+      indicators.value.isAdd = false;
+      indicators.value.isDelete = false;
+      formData.value.id = "";
+      managerMod.value.managerId = "";
+    },
+    (err) => {
+      request.value = false;
+      errorMessage.value.error = err;
+    }
+  );
 };
 
 //при загрузке делаем запрос к серверу
 onMounted(() => {
-	requestVacancyManagers();
+  requestVacancyManagers();
 });
 
 watch(
-	() => [indicators.value.isAdd, indicators.value.isDelete],
-	() => {
-		document.body.style.overflow =
-			indicators.value.isAdd || indicators.value.isDelete ? 'hidden' : '';
-	}
+  () => [indicators.value.isAdd, indicators.value.isDelete],
+  () => {
+    document.body.style.overflow =
+      indicators.value.isAdd || indicators.value.isDelete ? "hidden" : "";
+  }
 );
 </script>
 
 <style scoped>
 .manager-list__status-error {
-	color: var(--error-color);
+  color: var(--error-color);
 }
 .manager-list__status-spinner {
-	width: 50px;
+  width: 50px;
 }
 .manager-list__options-box {
-	margin: 20px 0;
-	display: flex;
-	align-items: center;
+  margin: 20px 0;
+  display: flex;
+  align-items: center;
 }
 .manager-list__select-box {
-	margin-left: 20px;
-	display: flex;
+  margin-left: 20px;
+  display: flex;
 }
 </style>
--- src/pages/vacancy/VacancyEdit/components/VacancyManagers/VacancyManagersItem.vue
+++ src/pages/vacancy/VacancyEdit/components/VacancyManagers/VacancyManagersItem.vue
@@ -1,81 +1,82 @@
 <template>
-	<div>
-		<div
-			@click="handleManagerClick"
-			class="manager-item__box"
-		>
-			<ManagerAssigned :ManagerText="manager.name" />
-			<ButtonIcon
-				class="manager-item__btn"
-				v-if="indicators.isHandled"
-				@click="handleManagerDeleteClick(manager)"
-			>
-				<template v-slot:icon>
-					<IconDelete class="manager-item__icon" />
-				</template>
-			</ButtonIcon>
-		</div>
-	</div>
+  <div>
+    <div @click="handleManagerClick(manager)" class="manager-item__box">
+      <ManagerAssigned :ManagerText="manager.name" class="manager-item__name" />
+      <ButtonIcon
+        class="manager-item__btn"
+        v-if="indicators.isHandled && props.managerMod.managerId === manager.id"
+        @click="handleManagerDeleteClick"
+      >
+        <template v-slot:icon>
+          <IconDelete class="manager-item__icon" />
+        </template>
+      </ButtonIcon>
+    </div>
+  </div>
 </template>
 
 <script setup>
-import ManagerAssigned from '@/components/ManagerAssigned.vue';
-import ButtonIcon from '@/components/ButtonIcon.vue';
-import IconDelete from '@/assets/icons/close.svg?component';
+import ManagerAssigned from "@/components/ManagerAssigned.vue";
+import ButtonIcon from "@/components/ButtonIcon.vue";
+import IconDelete from "@/assets/icons/close.svg?component";
 
 const props = defineProps({
-	// менеджер
-	manager: {
-		type: Object,
-		required: true,
-	},
-	// Индикаторы кнопок
-	indicators: {
-		type: Object,
-		required: true,
-	},
-	// Информация об изменяемом менеджере
-	managerMod: {
-		type: Object,
-		required: true,
-	},
+  // менеджер
+  manager: {
+    type: Object,
+    required: true,
+  },
+  // Индикаторы кнопок
+  indicators: {
+    type: Object,
+    required: true,
+  },
+  // Информация об изменяемом менеджере
+  managerMod: {
+    type: Object,
+    required: true,
+  },
 });
-
 //Отображение крестика для удаления
-const handleManagerClick = () => {
-	props.indicators.isHandled = !props.indicators.isHandled;
+const handleManagerClick = (manager) => {
+  props.indicators.isHandled === true &&
+  props.managerMod.managerId !== manager.id
+    ? (props.managerMod.managerId = manager.id)
+    : (props.indicators.isHandled = !props.indicators.isHandled);
+  props.managerMod.managerId = manager.id;
 };
 
 // Открытие попапа для удаления
-const handleManagerDeleteClick = (manager) => {
-	props.indicators.isDelete = true;
-	props.managerMod.managerId = manager.id;
+const handleManagerDeleteClick = () => {
+  props.indicators.isDelete = true;
 };
 </script>
 
 <style scoped>
 .manager-item__box {
-	display: flex;
-	user-select: none;
-	padding: 7px 13px;
-	border: 1px solid gray;
-	border-radius: 20px;
-	display: flex;
-	transition: opacity 0.3s ease, background-color 0.3s ease; /* Добавили transition для background-color */
-	cursor: pointer;
-	align-items: center;
+  display: flex;
+  user-select: none;
+  padding: 7px 13px;
+  border: 1px solid gray;
+  border-radius: 20px;
+  display: flex;
+  transition: opacity 0.3s ease, background-color 0.3s ease;
+  cursor: pointer;
+  align-items: center;
+  position: relative;
 }
 .manager-item__box:hover {
-	background-color: #d3d3d3; /* Цвет при наведении */
+  background-color: #d3d3d3;
 }
 .manager-item__icon {
-	position: relative;
-	margin-top: -9px;
-	height: 12px;
-	width: 12px;
-	display: block;
+  height: 12px;
+  width: 12px;
+  display: block;
+  position: absolute;
+  top: 4px;
+  right: 3px;
 }
 .manager-item__btn {
-	padding: 0 0 0 1px;
+  padding: 0 0 0 1px;
 }
 </style>
