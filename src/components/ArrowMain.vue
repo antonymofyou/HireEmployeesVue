526c48c31272635ea2363e26c08cc9394f43e1f6
Sergeev Danila <Danilasergeev2003@icloud.com>
2024-08-08 05:41:18 +0300

    add all functional

--- src/components/ArrowMain.vue
+++ src/components/ArrowMain.vue
@@ -1,56 +1,48 @@
 <template>
-  <svg :width="width" :height="height" :viewBox="viewBox" fill="none" xmlns="http://www.w3.org/2000/svg">
-  <g :transform="transform"> 
-    <path d="M12 4V20M12 4L8 8M12 4L16 8" :stroke="color" :stroke-width="thickness" stroke-linecap="round" stroke-linejoin="round"/>
+  <svg :width="size" :height="size" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <g :transform="props.direction === 'up' ? `rotate(${Number(props.angle)} 12 12)` : `rotate( ${Number(props.angle) + 90} 12 12)`"> 
+    <path d="M12 1V23M12 1L7 6M12 1L17 6" :stroke="color" :stroke-width="thickness" stroke-linecap="round" stroke-linejoin="round"/>
   </g>
 </svg>
 </template>
 
 <script setup>
-import {watch} from 'vue'
 const props = defineProps({
+  // Начальное направление up, right
     direction: {
       type: String,
-      default: 'up', // up, right
+      default: 'up'
     },
-    length: {
+
+  // Размер
+    size: {
       type: String,
       default: 50,
     },
+  // Толщина
     thickness: {
       type: String,
       default: 5,
     },
+  // Цвет
     color: {
       type: String,
       default: '#000',
     },
+  // Угол поворота
     angle: {
       type: String,
       default: 0,
     },
 
 })
-   const width = Number(props.length);
-   const height =  Number(props.length);
-   const transform = props.direction === "up" ? `rotate(${Number(props.angle)} 12 12)` : `rotate( ${Number(props.angle) + 90} 12 12)`;
-   const viewBox = props.direction === "up" ? '0 0 24 24' : '0 0 24 24'
-
-   watch(
-  () => props.direction,
-  (newDirection) => {
-    console.log(`Direction changed to: ${newDirection}`);
-  }
-);
 
 </script>
 
 <style scoped>
 svg {
   display: block;
+  overflow: visible;
 }
 </style>
 
-
-<!-- <Arrow direction="up" length = 200 thickness= "2" color="red" angle="0" />
-<Arrow direction="right" length = 200 thickness= "2" color="red" angle="0" /> -->
