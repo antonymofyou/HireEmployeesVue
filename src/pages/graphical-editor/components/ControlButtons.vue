32fff2ee8b51a6abd280d11e3e2a38a5436a4167
Matviei <matviei.maksimov@bk.ru>
2024-08-16 14:44:24 +0700

    Сделал выравнивание селектом

--- src/pages/graphical-editor/components/ControlButtons.vue
+++ src/pages/graphical-editor/components/ControlButtons.vue
@@ -24,54 +24,6 @@
         >
             <UnderlineIcon />
         </ControlButton>
-        <ControlButton 
-            class="control-buttons__button" 
-            :disabled="disabled"
-            :active="props.activeShape.editor?.isActive({'textAlign': 'left'})"
-            @click="props.activeShape.editor?.chain().focus().setTextAlign('left').run()"
-        >
-            <AlignLeftIcon />
-        </ControlButton>
-        <ControlButton 
-            class="control-buttons__button" 
-            :disabled="disabled"
-            :active="props.activeShape.editor?.isActive({'textAlign': 'center'})"
-            @click="props.activeShape.editor?.chain().focus().setTextAlign('center').run()"
-        >
-            <AlignCenterIcon />
-        </ControlButton>
-        <ControlButton 
-            class="control-buttons__button" 
-            :disabled="disabled"
-            :active="props.activeShape.editor?.isActive({'textAlign': 'right'})"
-            @click="props.activeShape.editor?.chain().focus().setTextAlign('right').run()"
-        >
-            <AlignRightIcon />
-        </ControlButton>
-        <ControlButton 
-            class="control-buttons__button" 
-            :disabled="disabled"
-            :active="props.activeShape.shape?.textVerticalAlignment == 'bottom'"
-            @click="emits('updateShape', props.activeShape.id, 'textVerticalAlignment', 'bottom')"
-        >
-            <VerticalAlignBottom />
-        </ControlButton>
-        <ControlButton 
-            class="control-buttons__button" 
-            :disabled="disabled"
-            :active="props.activeShape.shape?.textVerticalAlignment == 'center'"
-            @click="emits('updateShape', props.activeShape.id, 'textVerticalAlignment', 'center')"
-        >
-            <VerticalAlignCenter />
-        </ControlButton>
-        <ControlButton 
-            class="control-buttons__button" 
-            :disabled="disabled"
-            :active="props.activeShape.shape?.textVerticalAlignment == 'top'"
-            @click="emits('updateShape', props.activeShape.id, 'textVerticalAlignment', 'top')"
-        >
-            <VerticalAlignTop />
-        </ControlButton>
         <ColorPicker
             class="control-buttons__color-picker"
             :disabled="disabled"
@@ -84,6 +36,32 @@
             :font-size="parseInt(props.activeShape.editor?.getAttributes('textStyle').fontSize) || 16"
             @update:font-size="props.activeShape.editor?.chain().focus().setFontSize($event + 'px').run()"            
         />
+        <SelectMain
+            :options="optionsHorizontalAlign"
+            :model-value="currentHorizontalAlign"
+            @update:model-value="handlerHorizontalAlign"
+            :disabled='disabled'
+        >
+            <template #option="{ option }">  
+                <component :is="iconsHorizontalAlign[option.id]"></component>
+            </template>
+            <template #trigger="{ selected }">
+                <component :is="iconsHorizontalAlign[selected.id]"></component>
+            </template>
+        </SelectMain>
+        <SelectMain
+            :options="optionsVerticalAlign"
+            :model-value="currentVerticalAlign"
+            @update:model-value="handlerVerticalAlign"
+            :disabled='disabled'
+        >
+            <template #option="{ option }">  
+                <component :is="iconsVerticalAlign[option.id]"></component>
+            </template>
+            <template #trigger="{ selected }">
+                <component :is="iconsVerticalAlign[selected.id]"></component>
+            </template>
+        </SelectMain>
     </div>
 </template>
 
@@ -94,6 +72,7 @@ import { defineProps, defineEmits, computed } from 'vue';
 import ControlButton from './ControlButton.vue';
 import ColorPicker from './ColorPicker.vue';
 import FontSizePicker from './FontSizePicker.vue';
+import SelectMain from '@/components/SelectMain.vue';
 
 import BoldIcon from 'vue-material-design-icons/FormatBold.vue'
 import ItalicIcon from 'vue-material-design-icons/FormatItalic.vue'
@@ -105,29 +84,108 @@ import VerticalAlignBottom from 'vue-material-design-icons/FormatVerticalAlignBo
 import VerticalAlignCenter from 'vue-material-design-icons/FormatVerticalAlignCenter.vue'
 import VerticalAlignTop from 'vue-material-design-icons/FormatVerticalAlignTop.vue'
 
-/**
- * 
- * Объект с параметрами активной фигуры (id фигуры, экземпляр редактора (editor) , объект с параметрами фигуры)
- * 
- */
 const props = defineProps({
     activeShape: {
         type: Object,
         required: true,
     }
 });
-/**
- * 
- * updateShape(ид-фигуры, обновляемый ключ , значение)
- * 
- */
 const emits = defineEmits({
     updateShape: null
-})
-
+});
 const disabled = computed(() => {
     return !props.activeShape.editor;
-})
+});
+
+// HorizontalAlign
+
+const listItemsHorizontalAlign = [
+    {
+        id: 0,
+        name: 'left',
+        icon: AlignLeftIcon
+    },
+    {
+        id: 1,
+        name: 'center',
+        icon: AlignCenterIcon
+    },
+    {
+        id: 2,
+        name: 'right',
+        icon: AlignRightIcon
+    },
+];
+const optionsHorizontalAlign = listItemsHorizontalAlign.map(({id, name}) => {
+    return {
+        id,
+        name
+    }
+});
+const iconsHorizontalAlign = listItemsHorizontalAlign.reduce((acc, item) => {
+    acc[item.id] = item.icon;
+    
+    return acc;
+}, {});
+const currentHorizontalAlign = computed(() => {
+    const name = props.activeShape.editor?.getAttributes('paragraph').textAlign;
+    const { id } = optionsHorizontalAlign?.filter(item => item.name == name)[0] || {};
+
+    return id || 0;
+});
+
+function handlerHorizontalAlign(id) {
+    if (!props.activeShape.id) return;
+
+    const { name } = optionsHorizontalAlign?.filter(item => item.id == id)[0] || {};
+
+    props.activeShape.editor?.chain().focus().setTextAlign(name).run();
+}
+
+// VerticalAlign
+
+const listItemsVerticalAlign = [
+    {
+        id: 0,
+        name: 'bottom',
+        icon: VerticalAlignBottom
+    },
+    {
+        id: 1,
+        name: 'center',
+        icon: VerticalAlignCenter
+    },
+    {
+        id: 2,
+        name: 'top',
+        icon: VerticalAlignTop
+    },
+];
+const optionsVerticalAlign = listItemsVerticalAlign.map(({id, name}) => {
+    return {
+        id,
+        name
+    }
+});
+const iconsVerticalAlign = listItemsVerticalAlign.reduce((acc, item) => {
+    acc[item.id] = item.icon;
+    
+    return acc;
+}, {});
+const currentVerticalAlign = computed(() => {
+    const name = props.activeShape.shape?.textVerticalAlignment;
+    const { id } = optionsVerticalAlign?.filter(item => item.name == name)[0] || {};
+
+    return id || 0;
+});
+
+function handlerVerticalAlign(id) {
+    if (!props.activeShape.id) return;
+
+    const { name } = optionsVerticalAlign?.filter(item => item.id == id)[0] || {};
+
+    emits('updateShape', props.activeShape.id, 'textVerticalAlignment', name);
+}
 
 </script>
 
@@ -152,6 +210,53 @@ const disabled = computed(() => {
 .control-buttons__font-size-picker {
     width: 70px;
     font-size: 20px;
+}
+
+.select-box-main {
+    flex: 0 0 44px;
+}
+
+.select-box-main.active:deep(.selected-main) {
+    background-color: var(--transparent-blue);
+    color: var(--milk) !important;
+}
+
+.select-box-main.disabled:deep(.selected-main) {
+    opacity: 0.5;
+}
+
+.select-box-main:deep(.selected-main) {
+    background-color: var(--milk);
+    padding: 8px;
+    color: var(--mine-shaft);
+    transition: 0.2s ease;
+}
+
+.select-box-main:deep(.selected-main::after) {
+    display: none;
+}
+
+.select-box-main:deep(.options-container-main) {
+    top: auto;
+    bottom: 0;
+    display: flex;
+    box-shadow: 0px 2.5px 5px 1px rgba(34, 60, 80, 0.2);
+    transform: translateY(100%);
+    border: none;
+    background-color: var(--milk);
+    padding: 8px;
+}
+
+.select-box-main:deep(.option-main) {
+    background-color: var(--milk);
+    border-radius: 8px;
+    color: var(--mine-shaft);
+    transition: 0.2s ease;
+}
+
+.select-box-main:deep(.option-main.selected) {
+    background-color: var(--transparent-blue);
+    color: var(--milk) !important;
 }
 
 </style>
