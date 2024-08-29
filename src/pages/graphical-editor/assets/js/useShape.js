154d45ff4999b27b98f9403184978e3771be9565
Shustikus <shustikus5@gmail.com>
2024-08-29 17:35:47 +0300

    добавлены комментарии

--- src/pages/graphical-editor/assets/js/useShape.js
+++ src/pages/graphical-editor/assets/js/useShape.js
@@ -17,10 +17,12 @@ export function useShape(emits, props) {
     let draggingFrame;
     let resizingFrame;
 
+    // Функция для обновления свойств фигуры
     const updateShape = (updates) => {
         Object.entries(updates).forEach(([key, value]) => emits('updateShape', props.params.id, key, value));
     };
 
+    // Начало перемещения
     const startDragging = (event) => {
         if (!isEditMode.value) return;
 
@@ -32,6 +34,7 @@ export function useShape(emits, props) {
         document.addEventListener('mouseup', stopDragging);
     };
 
+    // Функция для перемещения
     const onDragging = (event) => {
         if (isDragging.value) {
             cancelAnimationFrame(draggingFrame);
@@ -40,17 +43,20 @@ export function useShape(emits, props) {
                 const newX = event.clientX - offsetX.value;
                 const newY = event.clientY - offsetY.value;
 
+                // Обновляем свойства фигуры
                 updateShape({x: newX, y: newY});
             });
         }
     };
 
+    // Завершение процесса перемещения
     const stopDragging = () => {
         isDragging.value = false;
         document.removeEventListener('mousemove', onDragging);
         document.removeEventListener('mouseup', stopDragging);
     };
 
+    // Начало изменения размера
     const startResizing = (handle) => {
         if (handle.isRotateHandle) {
             startRotating(event);
@@ -69,6 +75,7 @@ export function useShape(emits, props) {
         }
     };
 
+    // Функция для изменения размера
     const onResizing = (event) => {
         if (isResizing.value && resizingHandle) {
             cancelAnimationFrame(resizingFrame);
@@ -79,6 +86,7 @@ export function useShape(emits, props) {
                 let newX = startX.value;
                 let newY = startY.value;
 
+                // Обработка изменения размера по горизонтали
                 if (resizingHandle.position.includes('left')) {
                     newWidth = startWidth.value - (event.clientX - offsetX.value);
                     newX = newWidth > 0 ? startX.value + (event.clientX - offsetX.value) : startX.value + startWidth.value;
@@ -91,6 +99,7 @@ export function useShape(emits, props) {
                     }
                 }
 
+                // Обработка изменения размера по вертикали
                 if (resizingHandle.position.includes('top')) {
                     newHeight = startHeight.value - (event.clientY - offsetY.value);
                     newY = newHeight > 0 ? startY.value + (event.clientY - offsetY.value) : startY.value + startHeight.value;
@@ -103,11 +112,13 @@ export function useShape(emits, props) {
                     }
                 }
 
+                // Обновляем свойства фигуры
                 updateShape({width: newWidth, height: newHeight, x: newX, y: newY});
             });
         }
     };
 
+    // Завершение процесса изменения размера
     const stopResizing = () => {
         isResizing.value = false;
         resizingHandle = null;
@@ -115,6 +126,7 @@ export function useShape(emits, props) {
         document.removeEventListener('mouseup', stopResizing);
     };
 
+    // Начало вращения
     const startRotating = (event) => {
         isRotating.value = true;
         startX.value = event.clientX;
@@ -124,20 +136,24 @@ export function useShape(emits, props) {
         document.addEventListener('mouseup', stopRotating);
     };
 
+    // Функция для вращения
     const onRotating = (event) => {
         if (isRotating.value) {
             const centerX = props.params.x + props.params.width / 2;
             const centerY = props.params.y + props.params.height / 2;
             let angle = Math.atan2(event.clientY - centerY, event.clientX - centerX) * (180 / Math.PI);
 
+            // Привязка углов вращения, если зажата клавиша Shift
             if (event.shiftKey) {
                 angle = Math.round(angle / 15) * 15;
             }
 
+            // Обновляем свойства фигуры
             updateShape({rotation: angle});
         }
     };
 
+    // Завершение процесса вращения объекта
     const stopRotating = () => {
         isRotating.value = false;
         document.removeEventListener('mousemove', onRotating);
