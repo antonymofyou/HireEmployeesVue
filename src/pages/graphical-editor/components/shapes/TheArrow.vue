70e7e37647eaea34fc172633a25c763bda465aea
Shustikus <shustikus5@gmail.com>
2024-08-29 17:29:26 +0300

    вынесена логика для объектов, пофикшена ошибка когда подлагивал объект при изменении размера/перемещении

--- src/pages/graphical-editor/components/shapes/TheArrow.vue
+++ src/pages/graphical-editor/components/shapes/TheArrow.vue
@@ -57,8 +57,8 @@
 </template>
   
 <script setup>
-
-import {computed, onBeforeUnmount, ref} from 'vue';
+import {computed, onBeforeUnmount} from 'vue';
+import { useShape } from '../../assets/js/useShape';
 
 const props = defineProps({
     params: {
@@ -75,15 +75,7 @@ const props = defineProps({
     }
 });
 const emits = defineEmits(['updateShape', 'select-shape']);
-const isDragging = ref(false);
-const isResizing = ref(false);
-const isRotating = ref(false);
-const offsetX = ref(0);
-const offsetY = ref(0);
-const startX = ref(0);
-const startY = ref(0);
-const startWidth = ref(0);
-const startHeight = ref(0);
+
 const isEditMode = computed(() => {
   return props.mode.value === props.mode._edit;
 });
@@ -109,129 +101,13 @@ const handles = [
   { position: 'rotate', cursor: 'url("@/assets/icons/rotate.svg?component"), auto', isRotateHandle: true },
 ];
 
-// Выбор объекта
+const { startDragging, stopDragging, startResizing, stopResizing, stopRotating} = useShape(emits, props);
+
 const selectArrow = () => {
   emits('select-shape', {
     id: props.params.id,
   });
 }
-
-// Начало перемещения
-const startDragging = (event) => {
-  if (!isEditMode.value) return;
-
-  isDragging.value = true;
-  offsetX.value = event.clientX - props.params.x;
-  offsetY.value = event.clientY - props.params.y;
-
-  document.addEventListener('mousemove', onDragging);
-  document.addEventListener('mouseup', stopDragging);
-};
-
-// Функция для перемещения
-const onDragging = (event) => {
-  if (isDragging.value) {
-    const newX = event.clientX - offsetX.value;
-    const newY = event.clientY - offsetY.value;
-
-    emits('updateShape', props.params.id, 'x', newX);
-    emits('updateShape', props.params.id, 'y', newY);
-  }
-};
-
-// Завершение процесса перемещения
-const stopDragging = () => {
-  isDragging.value = false;
-  document.removeEventListener('mousemove', onDragging);
-  document.removeEventListener('mouseup', stopDragging);
-};
-
-// Начало изменения размера
-const startResizing = (handle) => {
-  if (handle.isRotateHandle) {
-    startRotating(event);
-  } else {
-    isResizing.value = true;
-    startX.value = props.params.x;
-    startY.value = props.params.y;
-    startWidth.value = props.params.width;
-    startHeight.value = props.params.height;
-    offsetX.value = event.clientX;
-    offsetY.value = event.clientY;
-
-    document.addEventListener('mousemove', onResizing(handle));
-    document.addEventListener('mouseup', stopResizing);
-  }
-};
-
-//Функция для изменения размера
-const onResizing = (handle) => (event) => {
-  if (isResizing.value) {
-    let newWidth = startWidth.value;
-    let newX = startX.value;
-
-    // Обработка изменения размера по горизонтали
-    if (handle.position.includes('left')) {
-      newWidth = startWidth.value - (event.clientX - offsetX.value);
-      if (newWidth > 0) {
-        newX = startX.value + (event.clientX - offsetX.value);
-      } else {
-        newX = startX.value + startWidth.value;
-        newWidth = Math.abs(newWidth);
-      }
-    } else if (handle.position.includes('right')) {
-      newWidth = startWidth.value + (event.clientX - offsetX.value);
-      if (newWidth < 0) {
-        newX = startX.value + newWidth;
-        newWidth = Math.abs(newWidth);
-      }
-    }
-
-    // Обновляем свойства фигуры
-    emits('updateShape', props.params.id, 'width', newWidth);
-    emits('updateShape', props.params.id, 'x', newX);
-  }
-};
-
-// Завершение процесса изменения размера
-const stopResizing = () => {
-  isResizing.value = false;
-  document.removeEventListener('mousemove', onResizing);
-  document.removeEventListener('mouseup', stopResizing);
-}
-
-// Начало вращения
-const startRotating = () => {
-  isRotating.value = true;
-  startX.value = event.clientX;
-  startY.value = event.clientY;
-
-  document.addEventListener('mousemove', onRotating);
-  document.addEventListener('mouseup', stopRotating);
-};
-
-// Функция для вращения
-const onRotating = (event) => {
-  if (isRotating.value) {
-    const centerX = props.params.x + props.params.width / 2;
-    const centerY = props.params.y + props.params.height / 2;
-    let angle = Math.atan2(event.clientY - centerY, event.clientX - centerX) * (180 / Math.PI);
-
-    // Привязка углов вращения, если зажата клавиша Shift
-    if (event.shiftKey) {
-      angle = Math.round(angle / 15) * 15;
-    }
-
-    emits('updateShape', props.params.id, 'rotation', angle);
-  }
-};
-
-// Завершение процесса вращения объекта
-const stopRotating = () => {
-  isRotating.value = false;
-  document.removeEventListener('mousemove', onRotating);
-  document.removeEventListener('mouseup', stopRotating);
-};
 
 onBeforeUnmount(() => {
   stopDragging();
--- src/pages/graphical-editor/components/shapes/TheRectangle.vue
+++ src/pages/graphical-editor/components/shapes/TheRectangle.vue
@@ -26,8 +26,9 @@
 </template>
 
 <script setup>
-import {defineProps, computed, onBeforeUnmount, ref, watch, onMounted} from 'vue';
+import {defineProps, computed, onMounted, onBeforeUnmount, ref, watch} from 'vue';
 import { useEditor, EditorContent } from '@tiptap/vue-3'
+import { useShape } from '../../assets/js/useShape'; // Импорт общего хука
 import StarterKit from '@tiptap/starter-kit'
 import Underline from '@tiptap/extension-underline'
 import TextAlign from '@tiptap/extension-text-align'
@@ -58,15 +59,6 @@ const props = defineProps({
     }
 });
 const emits = defineEmits(['updateShape', 'select-shape', 'change-mode']);
-const isDragging = ref(false);
-const isResizing = ref(false);
-const isRotating = ref(false);
-const offsetX = ref(0);
-const offsetY = ref(0);
-const startX = ref(0);
-const startY = ref(0);
-const startWidth = ref(0);
-const startHeight = ref(0);
 
 const paramsTextVerticalAlignment = {
     'top': 'flex-start',
@@ -132,7 +124,8 @@ watch(() => isTextMode.value, (newValue) => {
 });
 
 // Список манипуляторов
-const handles = [
+const handles  = computed(() => {
+  return [
   { position: 'top-left', cursor: 'nwse-resize' },
   { position: 'top-right', cursor: 'nesw-resize' },
   { position: 'bottom-left', cursor: 'nesw-resize' },
@@ -142,7 +135,10 @@ const handles = [
   { position: 'top', cursor: 'ns-resize' },
   { position: 'bottom', cursor: 'ns-resize' },
   { position: 'rotate', cursor: 'url("@/assets/icons/rotate.svg?component"), auto', isRotateHandle: true },
-];
+]
+});
+
+const { startDragging, stopDragging, startResizing, stopResizing, stopRotating} = useShape(emits, props);
 
 // Выбор объекта
 const selectRectangle = () => {
@@ -165,144 +161,6 @@ const toggleTextEditMode = () => {
   } else {
     enterTextEditMode();
   }
-};
-
-// Начало перемещения
-const startDragging = (event) => {
-  if (!isEditMode.value) return;
-
-  isDragging.value = true;
-  offsetX.value = event.clientX - props.params.x;
-  offsetY.value = event.clientY - props.params.y;
-
-  document.addEventListener('mousemove', onDragging);
-  document.addEventListener('mouseup', stopDragging);
-};
-
-// Функция для перемещения
-const onDragging = (event) => {
-  if (isDragging.value) {
-    const newX = event.clientX - offsetX.value;
-    const newY = event.clientY - offsetY.value;
-
-    emits('updateShape', props.params.id, 'x', newX);
-    emits('updateShape', props.params.id, 'y', newY);
-  }
-};
-
-// Завершение процесса перемещения
-const stopDragging = () => {
-  isDragging.value = false;
-  document.removeEventListener('mousemove', onDragging);
-  document.removeEventListener('mouseup', stopDragging);
-};
-
-// Начало изменения размера
-const startResizing = (handle) => {
-  if (handle.isRotateHandle) {
-    startRotating(event);
-  } else {
-  isResizing.value = true;
-  startX.value = props.params.x;
-  startY.value = props.params.y;
-  startWidth.value = props.params.width;
-  startHeight.value = props.params.height;
-  offsetX.value = event.clientX;
-  offsetY.value = event.clientY;
-
-  document.addEventListener('mousemove', onResizing(handle));
-  document.addEventListener('mouseup', stopResizing);
-  }
-};
-
-//Функция для изменения размера
-const onResizing = (handle) => (event) => {
-  if (isResizing.value) {
-    let newWidth = startWidth.value;
-    let newHeight = startHeight.value;
-    let newX = startX.value;
-    let newY = startY.value;
-
-    // Обработка изменения размера по горизонтали
-    if (handle.position.includes('left')) {
-      newWidth = startWidth.value - (event.clientX - offsetX.value);
-      if (newWidth > 0) {
-        newX = startX.value + (event.clientX - offsetX.value);
-      } else {
-        newX = startX.value + startWidth.value;
-        newWidth = Math.abs(newWidth);
-      }
-    } else if (handle.position.includes('right')) {
-      newWidth = startWidth.value + (event.clientX - offsetX.value);
-      if (newWidth < 0) {
-        newX = startX.value + newWidth;
-        newWidth = Math.abs(newWidth);
-      }
-    }
-
-    // Обработка изменения размера по вертикали
-    if (handle.position.includes('top')) {
-      newHeight = startHeight.value - (event.clientY - offsetY.value);
-      if (newHeight > 0) {
-        newY = startY.value + (event.clientY - offsetY.value);
-      } else {
-        newY = startY.value + startHeight.value;
-        newHeight = Math.abs(newHeight);
-      }
-    } else if (handle.position.includes('bottom')) {
-      newHeight = startHeight.value + (event.clientY - offsetY.value);
-      if (newHeight < 0) {
-        newY = startY.value + newHeight;
-        newHeight = Math.abs(newHeight);
-      }
-    }
-
-    // Обновляем свойства фигуры
-    emits('updateShape', props.params.id, 'width', newWidth);
-    emits('updateShape', props.params.id, 'height', newHeight);
-    emits('updateShape', props.params.id, 'x', newX);
-    emits('updateShape', props.params.id, 'y', newY);
-  }
-};
-
-// Завершение процесса изменения размера
-const stopResizing = () => {
-  isResizing.value = false;
-  document.removeEventListener('mousemove', onResizing);
-  document.removeEventListener('mouseup', stopResizing);
-}
-
-// Начало вращения
-const startRotating = (event) => {
-  isRotating.value = true;
-  startX.value = event.clientX;
-  startY.value = event.clientY;
-
-  document.addEventListener('mousemove', onRotating);
-  document.addEventListener('mouseup', stopRotating);
-};
-
-// Функция для вращения
-const onRotating = (event) => {
-  if (isRotating.value) {
-    const centerX = props.params.x + props.params.width / 2;
-    const centerY = props.params.y + props.params.height / 2;
-    let angle = Math.atan2(event.clientY - centerY, event.clientX - centerX) * (180 / Math.PI);
-
-    // Привязка углов вращения, если зажата клавиша Shift
-    if (event.shiftKey) {
-      angle = Math.round(angle / 15) * 15;
-    }
-
-    emits('updateShape', props.params.id, 'rotation', angle);
-  }
-};
-
-// Завершение процесса вращения объекта
-const stopRotating = () => {
-  isRotating.value = false;
-  document.removeEventListener('mousemove', onRotating);
-  document.removeEventListener('mouseup', stopRotating);
 };
 
  // Обработчик кликов вне объекта для снятия выделения
